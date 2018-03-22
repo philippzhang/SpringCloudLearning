4c048a02acd670ee27c3737ac80242f9826498ab
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2018-03-22 10:18:28 +0800

    commit
    Change-Id: I889b3dd1b23dfdb7a9f3f3e9080dd8620dd1be02

---
+++ chapter0/service-feign/src/main/java/com/springcloud/learn/service/SchedualServiceHiHystric.java
@@ -0,0 +1,14 @@
+package com.springcloud.learn.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by fangzhipeng on 2017/4/6.
+ */
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+	@Override
+	public String sayHiFromClientOne(String name) {
+		return "sorry " + name;
+	}
+}
